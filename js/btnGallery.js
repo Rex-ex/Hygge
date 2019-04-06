b557ec552028098fa5dcb57bd697b478d23f5801
Maro <rexex.webdev@gmail.com>
2019-04-06 14:35:16 +0200

    - new color palette

--- js/btnGallery.js
+++ js/btnGallery.js
@@ -1,9 +1,9 @@
 
-'use strict';
+'use strict'
 class TshirtCategory {
   constructor(cattegoryBTN, photoDIV) {
-    this.cattegoryBTN = cattegoryBTN;
-    this.photoDIV = photoDIV;
+    this.cattegoryBTN = cattegoryBTN
+    this.photoDIV = photoDIV
   }
 }
 
@@ -15,41 +15,41 @@ const Custom = new TshirtCategory('#btnCustom', '#photoCustom')
 
 const groupCategories = [Woman, Man, Preg, Kids, Custom]
 
-$('.photoCategory:not(:first)').hide();
+$('.photoCategory:not(:first)').hide()
 
 
 $(Woman.cattegoryBTN).click(function(){
-    $(Woman.photoDIV).toggle();
-    $(Man.photoDIV).hide();
-    $(Preg.photoDIV).hide();
-    $(Kids.photoDIV).hide();
-    $(Custom.photoDIV).hide();
+    $(Woman.photoDIV).toggle()
+    $(Man.photoDIV).hide()
+    $(Preg.photoDIV).hide()
+    $(Kids.photoDIV).hide()
+    $(Custom.photoDIV).hide()
 })
 $(Man.cattegoryBTN).click(function(){
-    $(Man.photoDIV).toggle();
-    $(Woman.photoDIV).hide();
-    $(Preg.photoDIV).hide();
-    $(Kids.photoDIV).hide();
-    $(Custom.photoDIV).hide();
+    $(Man.photoDIV).toggle()
+    $(Woman.photoDIV).hide()
+    $(Preg.photoDIV).hide()
+    $(Kids.photoDIV).hide()
+    $(Custom.photoDIV).hide()
 })
 $(Preg.cattegoryBTN).click(function(){
-    $(Preg.photoDIV).toggle();
-    $(Woman.photoDIV).hide();
-    $(Man.photoDIV).hide();
-    $(Kids.photoDIV).hide();
-    $(Custom.photoDIV).hide();
+    $(Preg.photoDIV).toggle()
+    $(Woman.photoDIV).hide()
+    $(Man.photoDIV).hide()
+    $(Kids.photoDIV).hide()
+    $(Custom.photoDIV).hide()
 })
 $(Kids.cattegoryBTN).click(function(){
-    $(Kids.photoDIV).toggle();
-    $(Woman.photoDIV).hide();
-    $(Man.photoDIV).hide();
-    $(Preg.photoDIV).hide();
-    $(Custom.photoDIV).hide();
+    $(Kids.photoDIV).toggle()
+    $(Woman.photoDIV).hide()
+    $(Man.photoDIV).hide()
+    $(Preg.photoDIV).hide()
+    $(Custom.photoDIV).hide()
 })
 $(Custom.cattegoryBTN).click(function(){
-    $(Custom.photoDIV).toggle();
-    $(Woman.photoDIV).hide();
-    $(Man.photoDIV).hide();
-    $(Kids.photoDIV).hide();
-    $(Preg.photoDIV).hide();
+    $(Custom.photoDIV).toggle()
+    $(Woman.photoDIV).hide()
+    $(Man.photoDIV).hide()
+    $(Kids.photoDIV).hide()
+    $(Preg.photoDIV).hide()
 })
